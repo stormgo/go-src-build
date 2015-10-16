130af143e2c5d2c39347abd1491fad5da380a5e2
Michael Angerman <stormasm@gmail.com>
2015-10-16 11:30:39 -0700

    delete unused files

--- src/mymake2.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo '##### Starting to run tests...'
-export GOROOT="$(cd .. && pwd)"
-GOROOT_BOOTSTRAP=${GOROOT_BOOTSTRAP:-$HOME/go1.4}
-
-if [ "$1" != "--no-banner" ]; then
-	"$GOTOOLDIR"/dist banner
-fi
